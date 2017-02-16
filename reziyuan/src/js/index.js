f3edcdccb62d1ad46cb15a12e4520b3bf2ac8748
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-02-17 04:00:14 +0800

    new

--- reziyuan/src/js/index.js
+++ reziyuan/src/js/index.js
@@ -94,10 +94,10 @@ $(function () {
         this.ulName = this.center + ' ul'
         this.liName = this.center + ' li'
         this.length = $(this.liName).length
-        this.numTol = [1, 2, 3, 4, 5]
-        this.layer_1 = 1
-        this.layer_2 = [2, this.numTol[(this.numTol.length - 1)]]
-        this.layer_3 = [3, this.numTol[(this.numTol.length - 2)]]
+        this.numTol = [0, 1, 2, 3, 4]
+        this.layer_1 = 0
+        this.layer_2 = [1, this.numTol[(this.numTol.length - 1)]]
+        this.layer_3 = [2, this.numTol[(this.numTol.length - 2)]]
         this.init = function () {
             if (this.length < 5) {
                 //补足空位
@@ -108,10 +108,299 @@ $(function () {
                 //装载数据
                 if (this.length !== 5) {
                     for (var i = 0; i < this.length - 5; i++)
-                    this.numTol.push(i + 6)
+                    this.numTol.push(i + 5)
                 }
+                this.layer_2 = [1, this.numTol[(this.numTol.length - 1)]]
+                this.layer_3 = [2, this.numTol[(this.numTol.length - 2)]]
             }
-            console.log(this.numTol)
+            //初始化样式
+            $($(this.liName)[this.layer_1]).css({
+                top: "15px",
+                left: "315px",
+                width: "215px",
+                height: "320px",
+                zIndex: "3",
+                lineHeight: "50px",
+                display: "block"
+            })
+            $($(this.liName)[this.layer_2[0]]).css({
+                top: "47px",
+                left: "515px",
+                width: "172px",
+                height: "256px",
+                zIndex: "2",
+                lineHeight: "40px",
+                opacity: "0.8",
+                display: "block"
+            })
+            $($(this.liName)[this.layer_2[1]]).css({
+                top: "47px",
+                left: "155px",
+                width: "172px",
+                height: "256px",
+                zIndex: "2",
+                lineHeight: "40px",
+                opacity: "0.8",
+                display: "block"
+            })
+            $($(this.liName)[this.layer_3[0]]).css({
+                top: "78px",
+                left: "680px",
+                width: "129px",
+                height: "192px",
+                zIndex: "1",
+                lineHeight: "30px",
+                opacity: "0.6",
+                display: "block"
+            })
+            $($(this.liName)[this.layer_3[1]]).css({
+                top: "78px",
+                left: "40px",
+                width: "129px",
+                height: "192px",
+                zIndex: "1",
+                lineHeight: "30px",
+                opacity: "0.6",
+                display: "block"
+            })
+        }
+        this.turnLeft = function () {
+            if (this.length > 5) {
+                if ($($(this.liName)[this.layer_3[1] - 1 < 0 ? this.length - 1 : this.layer_3[1] - 1]).css('display') === 'none') {
+                    $($(this.liName)[this.layer_3[1] - 1 < 0 ? this.length - 1 : this.layer_3[1] - 1]).css({display: 'block'})
+                }
+                if ($($(this.liName)[this.layer_3[0]]).css('display') === 'block') {
+                    $($(this.liName)[this.layer_3[0]]).css({display: 'none'})
+                }
+                this.layer_1 = this.layer_1 === 0 ? this.length - 1 : this.layer_1 - 1
+                this.layer_2 = [
+                    this.layer_2[0] === 0 ? this.length - 1 : this.layer_2[0] - 1,
+                    this.layer_2[1] === 0 ? this.length - 1 : this.layer_2[1] - 1
+                ]
+                this.layer_3 = [
+                    this.layer_3[0] === 0 ? this.length - 1 : this.layer_3[0] - 1,
+                    this.layer_3[1] === 0 ? this.length - 1 : this.layer_3[1] - 1
+                ]
+                $($(this.liName)[this.layer_1]).animate({
+                    zIndex: "3",
+                    top: "15px",
+                    left: "315px",
+                    width: "215px",
+                    height: "320px",
+                    lineHeight: "50px",
+                    opacity: "1"
+                })
+                $($(this.liName)[this.layer_2[0]]).animate({
+                    zIndex: "2",
+                    top: "47px",
+                    left: "515px",
+                    width: "172px",
+                    height: "256px",
+                    lineHeight: "40px",
+                    opacity: "0.8"
+                })
+                $($(this.liName)[this.layer_2[1]]).animate({
+                    zIndex: "2",
+                    top: "47px",
+                    left: "155px",
+                    width: "172px",
+                    height: "256px",
+                    lineHeight: "40px",
+                    opacity: "0.8"
+                })
+                $($(this.liName)[this.layer_3[0]]).animate({
+                    zIndex: "1",
+                    top: "78px",
+                    left: "680px",
+                    width: "129px",
+                    height: "192px",
+                    lineHeight: "30px",
+                    opacity: "0.6"
+                })
+                $($(this.liName)[this.layer_3[1]]).animate({
+                    zIndex: "1",
+                    top: "78px",
+                    left: "40px",
+                    width: "129px",
+                    height: "192px",
+                    lineHeight: "30px",
+                    opacity: "0.6"
+                })
+            } else {
+                this.layer_1 = this.layer_1 === 0 ? 4 : this.layer_1 - 1
+                this.layer_2 = [
+                    this.layer_2[0] === 0 ? 4 : this.layer_2[0] - 1,
+                    this.layer_2[1] === 0 ? 4 : this.layer_2[1] - 1
+                ]
+                this.layer_3 = [
+                    this.layer_3[0] === 0 ? 4 : this.layer_3[0] - 1,
+                    this.layer_3[1] === 0 ? 4 : this.layer_3[1] - 1
+                ]
+                $($(this.liName)[this.layer_1]).animate({
+                    zIndex: "3",
+                    top: "15px",
+                    left: "315px",
+                    width: "215px",
+                    height: "320px",
+                    lineHeight: "50px",
+                    opacity: "1"
+                })
+                $($(this.liName)[this.layer_2[0]]).animate({
+                    zIndex: "2",
+                    top: "47px",
+                    left: "515px",
+                    width: "172px",
+                    height: "256px",
+                    lineHeight: "40px",
+                    opacity: "0.8"
+                })
+                $($(this.liName)[this.layer_2[1]]).animate({
+                    zIndex: "2",
+                    top: "47px",
+                    left: "155px",
+                    width: "172px",
+                    height: "256px",
+                    lineHeight: "40px",
+                    opacity: "0.8"
+                })
+                $($(this.liName)[this.layer_3[0]]).animate({
+                    zIndex: "1",
+                    top: "78px",
+                    left: "680px",
+                    width: "129px",
+                    height: "192px",
+                    lineHeight: "30px",
+                    opacity: "0.6"
+                })
+                $($(this.liName)[this.layer_3[1]]).animate({
+                    zIndex: "1",
+                    top: "78px",
+                    left: "40px",
+                    width: "129px",
+                    height: "192px",
+                    lineHeight: "30px",
+                    opacity: "0.6"
+                })
+            }
+        }
+        this.turnRight = function () {
+            if (this.length > 5) {
+                if ($($(this.liName)[this.layer_3[0] + 1 > this.length - 1 ? 0 : this.layer_3[0] + 1]).css('display') === 'none') {
+                    $($(this.liName)[this.layer_3[0] + 1 > this.length - 1 ? 0 : this.layer_3[0] + 1]).css({display: 'block'})
+                }
+                if ($($(this.liName)[this.layer_3[1]]).css('display') === 'block') {
+                    $($(this.liName)[this.layer_3[1]]).css({display: 'none'})
+                }
+                this.layer_1 = this.layer_1 === this.length - 1 ? 0 : this.layer_1 + 1
+                this.layer_2 = [
+                    this.layer_2[0] === this.length - 1 ? 0 : this.layer_2[0] + 1,
+                    this.layer_2[1] === this.length - 1 ? 0 : this.layer_2[1] + 1
+                ]
+                this.layer_3 = [
+                    this.layer_3[0] === this.length - 1 ? 0 : this.layer_3[0] + 1,
+                    this.layer_3[1] === this.length - 1 ? 0 : this.layer_3[1] + 1
+                ]
+                $($(this.liName)[this.layer_1]).animate({
+                    zIndex: "3",
+                    top: "15px",
+                    left: "315px",
+                    width: "215px",
+                    height: "320px",
+                    lineHeight: "50px",
+                    opacity: "1"
+                })
+                $($(this.liName)[this.layer_2[0]]).animate({
+                    zIndex: "2",
+                    top: "47px",
+                    left: "515px",
+                    width: "172px",
+                    height: "256px",
+                    lineHeight: "40px",
+                    opacity: "0.8"
+                })
+                $($(this.liName)[this.layer_2[1]]).animate({
+                    zIndex: "2",
+                    top: "47px",
+                    left: "155px",
+                    width: "172px",
+                    height: "256px",
+                    lineHeight: "40px",
+                    opacity: "0.8"
+                })
+                $($(this.liName)[this.layer_3[0]]).animate({
+                    zIndex: "1",
+                    top: "78px",
+                    left: "680px",
+                    width: "129px",
+                    height: "192px",
+                    lineHeight: "30px",
+                    opacity: "0.6"
+                })
+                $($(this.liName)[this.layer_3[1]]).animate({
+                    zIndex: "1",
+                    top: "78px",
+                    left: "40px",
+                    width: "129px",
+                    height: "192px",
+                    lineHeight: "30px",
+                    opacity: "0.6"
+                })
+            } else {
+                this.layer_1 = this.layer_1 === 4 ? 0 : this.layer_1 + 1
+                this.layer_2 = [
+                    this.layer_2[0] === 4 ? 0 : this.layer_2[0] + 1,
+                    this.layer_2[1] === 4 ? 0 : this.layer_2[1] + 1
+                ]
+                this.layer_3 = [
+                    this.layer_3[0] === 4 ? 0 : this.layer_3[0] + 1,
+                    this.layer_3[1] === 4 ? 0 : this.layer_3[1] + 1
+                ]
+                $($(this.liName)[this.layer_1]).animate({
+                    zIndex: "3",
+                    top: "15px",
+                    left: "315px",
+                    width: "215px",
+                    height: "320px",
+                    lineHeight: "50px",
+                    opacity: "1"
+                })
+                $($(this.liName)[this.layer_2[0]]).animate({
+                    zIndex: "2",
+                    top: "47px",
+                    left: "515px",
+                    width: "172px",
+                    height: "256px",
+                    lineHeight: "40px",
+                    opacity: "0.8"
+                })
+                $($(this.liName)[this.layer_2[1]]).animate({
+                    zIndex: "2",
+                    top: "47px",
+                    left: "155px",
+                    width: "172px",
+                    height: "256px",
+                    lineHeight: "40px",
+                    opacity: "0.8"
+                })
+                $($(this.liName)[this.layer_3[0]]).animate({
+                    zIndex: "1",
+                    top: "78px",
+                    left: "680px",
+                    width: "129px",
+                    height: "192px",
+                    lineHeight: "30px",
+                    opacity: "0.6"
+                })
+                $($(this.liName)[this.layer_3[1]]).animate({
+                    zIndex: "1",
+                    top: "78px",
+                    left: "40px",
+                    width: "129px",
+                    height: "192px",
+                    lineHeight: "30px",
+                    opacity: "0.6"
+                })
+            }
         }
     }
 
@@ -123,14 +412,20 @@ $(function () {
         center: ".u-center"
     }
 
-
     /**实例化走马灯 */
     var rotate = new Rotate(configRT)
 
     /**运行 */
-    rotate.init()
-
-
+    function runRotate() {
+        rotate.init()
+        $(rotate.arrRight).on('click', function () {
+            rotate.turnRight()
+        })
+        $(rotate.arrLeft).on('click', function () {
+            rotate.turnLeft()
+        })
+    }
+    runRotate()    
     /**------------------------------------------------------------------------- */
 
 
