240adc05e3825a6db2f5ea50a9bf5f316025656d
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-03-01 12:37:24 +0800

    new

---
+++ reziyuan/src/js/buildresources.js
@@ -0,0 +1,19 @@
+$(function () {
+    //根据URL所带的type参数改变导航样式
+    var typeNum = window.location.search.substr(1)
+    $($('.m-myresource li~li')[0]).children('a').addClass('u-ul')
+    $($('.m-myresource li~li')[1]).children('a').addClass('u-dl')
+    $($('.m-myresource li~li')[2]).children('a').addClass('u-mf')
+    if (typeNum === 'type=1') {
+        $($('.m-myresource li~li')[0]).children('a').removeClass('u-ul').addClass('z-selected u-ulr')
+        $($('.m-myresource li~li')[0]).children('div').show()
+    } else if (typeNum === 'type=2') {
+        $($('.m-myresource li~li')[1]).children('a').removeClass('u-dl').addClass('z-selected u-dlr')
+        $($('.m-myresource li~li')[1]).children('div').show()
+    }else if (typeNum === 'type=3') {
+        $($('.m-myresource li~li')[2]).children('a').removeClass('u-mf').addClass('z-selected u-mfr')
+        $($('.m-myresource li~li')[2]).children('div').show()
+    }
+
+    console.log($($('.m-myresource li~li')[0]).children('a'))
+})
