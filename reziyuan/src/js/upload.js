d8cec586a9af736e2636ac69e40b00b2d43e4cbc
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-03-12 22:26:48 +0800

    new

--- reziyuan/src/js/upload.js
+++ reziyuan/src/js/upload.js
@@ -33,3 +33,26 @@ function imguploader(imgupload, showimg, info) {
         reader.readAsDataURL(file)
     })
 }
+
+function attuploader(attupload, info) {
+    $(attupload).change(function () {
+        if (!$(this).val()) {
+            $(info).html('没有选择文件...')
+            return
+        }
+        
+        var file = $(this).prop('files')[0],
+            size = (file.size / 1024).toFixed(1)
+        $(info).html(
+            '文件名称: ' + file.name + '<br>' +
+            '文件大小: ' + size + 'kb'
+        )
+        if (file.type !== 'image/jpeg' &&
+            file.type !== 'image/png' &&
+            file.type !== 'image/gif' &&
+            file.type !== 'image/jpg') {
+            $(info).html('不是有效的图片文件...')
+            return
+        }
+    })
+}
